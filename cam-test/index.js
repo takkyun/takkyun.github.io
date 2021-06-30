88bff8f1402e0cc3bf92ced591ba103d4b5afbc1
Takuya Otani <takuya.otani@serenebach.net>
2021-06-30 22:22:26 +1200

    add cam-test
    copy the code from https://techblog.kayac.com/webcamera-preview-for-ios11

---
+++ cam-test/index.js
@@ -0,0 +1,49 @@
+const medias = {
+  audio: false,
+  video: {
+    facingMode: {
+      exact: "environment"
+    }
+  }
+};
+const video  = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const promise = navigator.mediaDevices.getUserMedia(medias);
+
+let imgData;
+let data
+let ave;
+
+promise.then(successCallback)
+       .catch(errorCallback);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+  requestAnimationFrame(draw);
+};
+
+function errorCallback(err) {
+  alert(err);
+};
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  imgData = ctx.getImageData(0, 0, canvas.width,  canvas.height);
+  data = imgData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    ave = (data[i + 0] + data[i + 1] + data[i + 2]) / 3;
+
+    // data[i + 0] = 
+    // data[i + 1] = 
+    data[i + 2] = (ave > 255 / 2) ? 255 : (ave > 255 / 4) ? 127 : 0;
+    data[i + 3] = 255;
+  }
+
+  ctx.putImageData(imgData, 0, 0);
+  requestAnimationFrame(draw);
+}
